3957200cdd4b8d9887590a6cff1e9346dbd59dff
TG2408 <tanmaygupta2408@gmail.com>
2021-09-17 19:43:34 +0530

    scroll feature to update size added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 10;
 
 
 let isDrwaing = false;
@@ -20,17 +21,20 @@ function draw(ev) {
   ctx.moveTo(lastX, lastY);
   ctx.lineTo(ev.offsetX, ev.offsetY);
   ctx.stroke();
-  lastX = ev.offsetX;
-  lastY = ev.offsetY;
+  [lastX, lastY] = [ev.offsetX, lastY = ev.offsetY]
 }
 
 function setOffset(ev) {
-    isDrwaing = true;
-    lastX = ev.offsetX;
-    lastY = ev.offsetY;
+  isDrwaing = true;
+  [lastX, lastY] = [ev.offsetX, lastY = ev.offsetY]
 }
 
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mousedown', setOffset /*() => isDrwaing = true*/);
 canvas.addEventListener('mouseup', () => isDrwaing = false);
 canvas.addEventListener('mouseout', () => isDrwaing = false);
+
+canvas.addEventListener('wheel', e => {
+  if(e.deltaY > 0) ctx.lineWidth -= 3;
+  else ctx.lineWidth += 3;
+})
